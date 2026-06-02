59a564f7e11a190faa3f7ff2d8297508be10be15
CarlosGabriel629 <gabriel2009psantos@gmail.com>
2026-06-02 15:20:18 -0300

    subindo ex11.js

---
+++ ex11.js
@@ -0,0 +1,10 @@
+
+let numeros = [1, 2, 3, 4, 5];
+let dobro = [];
+
+for (let i = 0; i < numeros.length; i++) {
+    dobro.push(numeros[i] * 2);
+}
+
+console.log('Números: ' + numeros);
+console.log('Dobro: ' + dobro);
